1561bf7ba75339af1e45ade17027c1de9f778b78
Raimu0021 <raimu2326021923@gmail.com>
2024-05-14 09:59:21 +0900

    syuusei

--- PHP/common/header.php
+++ PHP/common/header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>サイドバー</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="header.css">
 </head>
 <body>
     <div class="sidebar">
