30943ace355e656c3c057a381ce1929e8f4c29b0
kyooo8 <kyosuke.7876@gmail.com>
2024-05-15 12:46:18 +0900

    変更

--- PHP/setting.php
+++ PHP/setting.php
@@ -1,1 +1,22 @@
-設定
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>設定ページ</title>
+    <link rel="stylesheet" href="../CSS/setting.css">
+</head>
+<body>
+    <div class="container">
+        <form action="change_email.php" method="get">
+            <button type="submit">メールアドレス変更</button>
+        </form>
+        <form action="change_password.php" method="get">
+            <button type="submit">パスワード変更</button>
+        </form>
+        <form action="delete_account.php" method="get">
+            <button type="submit">アカウント削除</button>
+        </form>
+    </div>
+</body>
+</html>
