ab2808a4c3d46784039f51d1cb61c9513e87ce9c
kyooo8 <kyosuke.7876@gmail.com>
2024-05-20 09:49:10 +0900

    a

--- PHP/common/db-connect.php
+++ PHP/common/db-connect.php
@@ -2,7 +2,7 @@
     const SERVER = 'mysql304.phy.lolipop.lan';
     const DBNAME = 'LAA1521164-copuru';
     const USER = 'LAA1521164';
-    const PASS = 'asojuku';
+    const PASS = 'tomodachi';
 
     $connect = 'mysql:host='. SERVER .";dbname=".  DBNAME .';charset=utf8';
 ?>
