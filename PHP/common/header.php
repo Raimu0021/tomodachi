309478ecb058a70e53a377bada9f5f2ebe877851
massan02 <tankful-secure0c@icloud.com>
2024-05-22 12:28:38 +0900

    font-awesomeの追加

--- PHP/common/header.php
+++ PHP/common/header.php
@@ -4,6 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>サイドバー</title>
     <link rel="stylesheet" href="../CSS/header.css">
+    <link rel ="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" 
         rel="stylesheet"
