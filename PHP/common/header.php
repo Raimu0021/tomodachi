1c4f2352038dffe9ffc2e7f1e4512200052c554c
kyooo8 <kyosuke.7876@gmail.com>
2024-05-22 12:05:11 +0900

    通知機能

--- PHP/common/header.php
+++ PHP/common/header.php
@@ -2,17 +2,8 @@
 <head>
     <meta charset="UTF-8">
     <link rel="stylesheet" href="../CSS/header.css">
-    <link
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" 
-        rel="stylesheet"
-        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" 
-        crossorigin="anonymous"
-    >
-    <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" 
-    crossorigin="anonymous"
-    ></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="sidebar">
@@ -21,34 +12,37 @@
         </a>
         <ul>
             <li><a href="./home.php">ホーム</a></li>
-            <li><a href="./notification.php" id="noti">通知</a></li>
+            <li><a href="#" id="noti">通知</a></li>
             <li><a href="./date.php">デート</a></li>
             <li><a href="./messege.php">メッセージ</a></li>
             <li><a href="./setting.php">設定</a></li>
             <li id="profile"><a href="./profile">プロフィール</a></li>
         </ul>
-        <div class="notification">
-            <?php require "./notification.php";?>
-        </div>
-
-
-        <style>
-            .notification {
-                display: none;
-            }
         
-        .open {
-            width: 250px;
-            transform: translatex(300px);
-            border: solid #DADADA 2px;
-        }
-        </style>
-
-        <script>
-            const noti = document.getElementaryById("noti");
-            noti.document.addEventlist("Click",()=>{
-                noti.classList.toggle("open");
-            })
-        </script>
     </div>
+    <div class="notification" style="display:none;">
+            <?php require "./notification.php";?>
+    </div>
+    <style>
+        .notification {
+            position: absolute;
+            width: 250px;
+            height: 100%;
+            top: 0;
+            border: solid #dadada 1px;
+        }
+    </style>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const notification = document.querySelector(".notification");
+            const nLink = document.getElementById("noti");
 
+            nLink.addEventListener("click", () => {
+                if (notification.style.display === 'block') {
+                    notification.style.display = 'none';
+                } else {
+                    notification.style.display = 'block';
+                }
+            });
+        });
+    </script>
--- PHP/notification.php
+++ PHP/notification.php
@@ -1,4 +1,2 @@
-<?php require 'common/header.php';?>
+
     <label>新規通知</label>
-</div>
-<?php require 'common/footer.php';?>
