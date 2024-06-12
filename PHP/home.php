4204d81e0283987850891c8cb97a3b022560993d
massan02 <tankful-secure0c@icloud.com>
2024-06-12 12:14:19 +0900

    サジェスト検索　フォームsubmit防止 空入力の際の学校表示をなくした

--- PHP/home.php
+++ PHP/home.php
@@ -18,12 +18,20 @@ $loggedInUser = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
 $(document).ready(function() {
     $('#school_name').on('input', function() {
         let text = $(this).val();
-        console.log(text);// 入力したもの
+        if (text === '') {
+            $('#school_predictions').html('');
+            return; // テキストが空の場合はここで処理を終了
+        }
+        console.log(text); // 入力したもの
         $.get('./common/searchSchool.php', {text: text}, function(data) {
             let schools = JSON.parse(data);
-            console.log(schools);//  API返り値
+            if (schools.length === 0) {
+                $('#school_predictions').html(''); // 結果が空の場合はリンクを表示しない
+                return;
+            }
+            console.log(schools); // API返り値
             let html = '';
-            for(var key in schools) {
+            for(let key in schools) {
                 if(schools.hasOwnProperty(key)) {
                     html += '<div><a href="search.php?school_id=' + schools[key].school_id + '">' + schools[key].school_name + '</a></div>';
                     console.log(schools[key].school_name);
@@ -31,6 +39,10 @@ $(document).ready(function() {
             }
             $('#school_predictions').html(html);
         });
+    });
+
+    $('form').on('submit', function(event) {
+        event.preventDefault(); // フォームのsubmitを防止
     });
 });
 </script>
