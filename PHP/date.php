befbc923f939e040719123b7f2ef8b4765a3fc40
sota022 <144978935+sota022@users.noreply.github.com>
2024-07-01 10:58:00 +0900

    date

--- PHP/date.php
+++ PHP/date.php
@@ -28,6 +28,27 @@ $loggedInUser = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
                 }
             }
         }
+        if($loggedInUser != null) {
+
+            $sql = "SELECT profile_image, user_name, date_of_birth, gender, school_id FROM users WHERE user_id = :liked_user_id AND is_private = 0";
+            $stmt = $conn->prepare($sql);
+            $stmt->bindParam(':user_id', $loggedInUser, PDO::PARAM_INT);
+            $stmt->execute();
+    
+            while($like = $stmt->fetch(PDO::FETCH_ASSOC)) {
+                $sql = "SELECT profile_image, user_name, date_of_birth, gender, school_id FROM users WHERE user_id = :liked_user_id";
+                $stmt2 = $conn->prepare($sql);
+                $stmt2->bindParam(':liked_user_id', $like['liked_user_id'], PDO::PARAM_INT);
+                $stmt2->execute();
+                
+                while($user = $stmt2->fetch(PDO::FETCH_ASSOC)) {
+                    echo "<h2>デート申請</h2>";
+                    echo '<div class="col-md-3 mb-4">';
+                    renderCard($user['profile_image'], $user['user_name'], $user['date_of_birth'], $user['gender'], $user['school_id']);
+                    echo '</div>';
+                }
+            }
+        }
     ?>
 
 <?php require './common/footer.php'; ?>
