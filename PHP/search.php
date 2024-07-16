b604415017a84e76e81fe41c5fa06fd5696080c2
massan02 <tankful-secure0c@icloud.com>
2024-07-16 10:36:17 +0900

    検索画面でコンポーネントが動作するよう引数を修正

--- PHP/search.php
+++ PHP/search.php
@@ -6,9 +6,10 @@ require './common/db-connect.php';
 require './common/searchSchool.php';
 $loggedInUser = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
 
+$user_id = $_SESSION['user_id'];
 $schoolId = $_GET['school_id'];
 
-$sql = "SELECT profile_image, user_name, date_of_birth, gender, school_id FROM users WHERE school_id = :school_id AND is_private = 0";
+$sql = "SELECT * FROM users WHERE school_id = :school_id AND is_private = 0";
 $stmt = $conn->prepare($sql);
 $stmt->bindParam(':school_id', $schoolId, PDO::PARAM_INT);
 $stmt->execute();
@@ -25,7 +26,7 @@ $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 if (count($results) > 0) {
     foreach ($results as $row) {
         echo '<div class="col-md-3 mb-4">';
-            renderCard($row['profile_image'], $row['user_name'], $row['date_of_birth'], $row['gender'], $row['school_id']);
+            renderCard($row['user_id'], $row['profile_image'], $row['user_name'], $row['date_of_birth'], $row['gender'], $row['school_id'], $user_id);
         echo '</div>';
     }
 } else {
