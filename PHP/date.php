f8ecca463af9c278d1b0bfe20e4113c13a9ca78e
massan02 <tankful-secure0c@icloud.com>
2024-07-23 10:50:44 +0900

    デート中のユーザーを表示しないよう変更 デート申請が来ているユーザーの部分のみ断るボタンが表示されるよう変更

--- PHP/date.php
+++ PHP/date.php
@@ -3,6 +3,7 @@ require 'common/header.php' ;
 require 'common/date_card_component.php';
 require './common/db-connect.php'; 
 $loggedInUser = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
+$hasDateRequest = false;
 ?>
 
         
@@ -33,13 +34,13 @@ $loggedInUser = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
             $loggedInUserId = $_SESSION['user_id'];
         
             // デート申請を送ってきたユーザーを取得
-            $dateRequestsSql = "SELECT u.* FROM users u JOIN dates d ON u.user_id = d.sender_id WHERE d.receiver_id = :loggedInUserId AND d.is_hidden = 0 ORDER BY RAND() LIMIT 8";
+            $dateRequestsSql = "SELECT u.* FROM users u JOIN dates d ON u.user_id = d.sender_id WHERE d.receiver_id = :loggedInUserId AND d.is_hidden = 0 AND u.currently_dating = 0 ORDER BY RAND() LIMIT 8";
             $stmt = $conn->prepare($dateRequestsSql);
             $stmt->execute(['loggedInUserId' => $loggedInUserId]);
             $dateRequestUsers = $stmt->fetchAll(PDO::FETCH_ASSOC);
         
             // いいねを送ってきたユーザーを取得
-            $likesSql = "SELECT u.* FROM users u JOIN likes l ON u.user_id = l.user_id WHERE l.liked_user_id = :loggedInUserId ORDER BY RAND() LIMIT 8";
+            $likesSql = "SELECT u.* FROM users u JOIN likes l ON u.user_id = l.user_id WHERE l.liked_user_id = :loggedInUserId AND u.currently_dating = 0 ORDER BY RAND() LIMIT 8";
             $stmt = $conn->prepare($likesSql);
             $stmt->execute(['loggedInUserId' => $loggedInUserId]);
             $usersWhoLikedLoggedInUser = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -55,7 +56,7 @@ $loggedInUser = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
             // 1. ログインユーザーがいいねしたユーザーの詳細を取得
             foreach($userIdsLikedByLoggedInUser as $like) {
                 
-                $sql = "SELECT profile_image, user_name, date_of_birth, gender, school_id FROM users WHERE user_id = :liked_user_id AND is_private = 0";
+                $sql = "SELECT profile_image, user_name, date_of_birth, gender, school_id FROM users WHERE user_id = :liked_user_id AND is_private = 0 AND currently_dating = 0";
                 $stmt2 = $conn->prepare($sql);
                 $stmt2->bindParam(':liked_user_id', $like['liked_user_id'], PDO::PARAM_INT);
                 $stmt2->execute();
@@ -65,25 +66,26 @@ $loggedInUser = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
                 while($user = $stmt2->fetch(PDO::FETCH_ASSOC)) {
                     
                     echo '<div class="col-md-3 mb-4">';
-                    renderDateCard($_SESSION['user_id'], $like['liked_user_id'], $user['profile_image'], $user['user_name'], $user['date_of_birth'], $user['gender'], $user['school_id']);
+                    renderDateCard($_SESSION['user_id'], $like['liked_user_id'], $user['profile_image'], $user['user_name'], $user['date_of_birth'], $user['gender'], $user['school_id'],$hasDateRequest);
                     echo '</div>';
                 }
             }
 
-            // 2. デート申請を送ってきたユーザーを取得
+            // 2. デート申請を送ってきたユーザーを取得　
             echo "<h2>デート申請を送ってきたユーザー</h2>";
+            $hasDateRequest = true;
             foreach($dateRequestUsers as $user) {
-                
                 echo '<div class="col-md-3 mb-4">';
-                renderDateCard($_SESSION['user_id'], $user['user_id'], $user['profile_image'], $user['user_name'], $user['date_of_birth'], $user['gender'], $user['school_id']);
+                renderDateCard($_SESSION['user_id'], $user['user_id'], $user['profile_image'], $user['user_name'], $user['date_of_birth'], $user['gender'], $user['school_id'],$hasDateRequest);
                 echo '</div>';
             }
+            $hasDateRequest = false;
 
             // 3. いいねを送ってきたユーザーを取得
             echo "<h2>いいねを送ってきたユーザー</h2>";
             foreach($usersWhoLikedLoggedInUser as $user) {
                 echo '<div class="col-md-3 mb-4">';
-                renderDateCard($_SESSION['user_id'], $user['user_id'], $user['profile_image'], $user['user_name'], $user['date_of_birth'], $user['gender'], $user['school_id']);
+                renderDateCard($_SESSION['user_id'], $user['user_id'], $user['profile_image'], $user['user_name'], $user['date_of_birth'], $user['gender'], $user['school_id'],$hasDateRequest);
                 echo '</div>';
             }
         }
@@ -111,6 +113,24 @@ $loggedInUser = isset($_SESSION['user_id']) ? $_SESSION['user_id'] : null;
             console.log('Success:', data);
         })
         .catch((error) => {
+            console.error('Error:', error);
+        });
+    }
+
+    function declineDate(sender_id, receiver_id){
+        // AJAXリクエストをバックエンドに送信
+        fetch('handler/date_decline_handler.php',{
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ sender_id: sender_id, receiver_id: receiver_id }),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:',data);
+        })
+        .catch((error) =>{
             console.error('Error:', error);
         });
     }
