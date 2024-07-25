1a0b5d75a1bb11bb34637723783579ab65b25f4b
massan02 <tankful-secure0c@icloud.com>
2024-07-25 12:27:51 +0900

    コメント追加

--- PHP/date_evaluation.php
+++ PHP/date_evaluation.php
@@ -1,7 +1,11 @@
+<!--
 問題
 ・ペアがこのサイトに訪れた際どのような画面を表示すべきか
 実施した対処法
-そもそも成功した際の処理が
+もう一方のペアが成功、または失敗を選んだ際、ペアも同じ処理を走らせる
+二人が同じ画面を開き、一人の処理が終了した際にもう一人が同じ処理を走らせ、予期しない処理が起こらないよう、
+handlerでエラーハンドリング（自身のcurrently_datingが0の場合などにエラーメッセージを返す等）の必要あり
+-->
 
 <?php
 session_start();
@@ -37,5 +41,10 @@ require './common/db-connect.php';
     </div>
 </div>
 
+<!-- 
 成功した場合
-それぞれの
+それぞれのcurrently_datingを0、is_privateを1に変更、date_success.phpに飛ばす
+
+失敗した場合
+それぞれのcurrently_datingを0、date_failure.phpに飛ばす
+-->
